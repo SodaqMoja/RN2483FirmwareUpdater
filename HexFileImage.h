7987907a11957720cd7700e20de4c65f0b92c026
Jan van Loenen <email@janvanloenen.nl>
2018-02-16 11:05:16 +0100

    add firmware version 1.0.4. for the RN2483

--- HexFileImage.h
+++ HexFileImage.h
@@ -8,6 +8,7 @@
 // Uncomment one of the following, and one only, not more, not less.
 //#define HEXFILE_RN2483_101
 //#define HEXFILE_RN2483_103
+//#define HEXFILE_RN2483_104
 //#define HEXFILE_RN2903AU_097rc7
 //#define HEXFILE_RN2903_098
 //#define HEXFILE_RN2903_103
@@ -16,6 +17,8 @@
 #include "HexFileImage2483_101.h"
 #elif defined(HEXFILE_RN2483_103)
 #include "HexFileImage2483_103.h"
+#elif defined(HEXFILE_RN2483_104)
+#include "HexFileImage2483_104.h"
 #elif defined(HEXFILE_RN2903AU_097rc7)
 #include "HexFileImage2903AU_097rc7.h"
 #elif defined(HEXFILE_RN2903_098)
